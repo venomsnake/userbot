70bb002490bb0132ab7f0b666a63dda2472bd186
Light Yagami <markrayon5@gmail.com>
2022-02-01 12:03:07 +0530

    Update ultroid.sh

--- ultroid.sh
+++ ultroid.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/TeamUltroid/Ultroid /root/TeamUltroid
-cp CREDS/.env /root/TeamUltroid/.env
+cp CREDS/config.env /root/TeamUltroid/config.env
 cd /root/TeamUltroid
 docker build . --rm --force-rm --compress --pull --file Dockerfile -t ultroid
 docker run --privileged --env-file .env --rm -i ultroid
