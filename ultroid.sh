6219b38d81b667aced93297884ed17458aecf715
Venom Snake <78187975+venomsnake@users.noreply.github.com>
2022-07-28 19:19:13 +0530

    test

--- ultroid.sh
+++ ultroid.sh
@@ -1,6 +1,7 @@
 git clone https://github.com/TeamUltroid/Ultroid /root/TeamUltroid
 cp CREDS/.env /root/TeamUltroid/.env
 cd /root/TeamUltroid
+sudo apt install ffmpeg
 pip3 install -U -r re*/st*/optional-requirements.txt
 pip3 install -U -r requirements.txt
 bash startup
